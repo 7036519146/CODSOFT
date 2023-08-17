1271cd8f44260a14daf39fff46f813d4539a20bb
NAVEENKUMAR <100122174+7036519146@users.noreply.github.com>
2023-08-17 19:54:05 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import messagebox
+import random
+import string
+
+
+entry_username = None
+entry_password_length = None
+entry_generated_password = None
+
+def generate_password():
+    global entry_generated_password
+    password_length = int(entry_password_length.get())
+    if password_length < 8:
+        messagebox.showwarning("Warning", "Password length should be at least 8 characters.")
+        return
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    generated_password = ''.join(random.choice(characters) for _ in range(password_length))
+    entry_generated_password.delete(0, tk.END)
+    entry_generated_password.insert(tk.END, generated_password)
+
+def accept_password():
+    global entry_username, entry_generated_password
+    username = entry_username.get()
+    password = entry_generated_password.get()
+    if not username or not password:
+        messagebox.showwarning("Warning", "Please enter username and generate a password.")
+        return
+
+
+    messagebox.showinfo("Accepted", f"Username: {username}\nPassword: {password}")
+
+def reset_fields():
+    global entry_username, entry_password_length, entry_generated_password
+    entry_username.delete(0, tk.END)
+    entry_password_length.delete(0, tk.END)
+    entry_generated_password.delete(0, tk.END)
+
+def main():
+    global entry_username, entry_password_length, entry_generated_password
+    root = tk.Tk()
+    root.title("Password Generator")
+
+
+    heading_label = tk.Label(root, text="Password Generator", font=("Helvetica", 16, "bold"))
+    heading_label.grid(row=0, column=0, columnspan=3, pady=20)
+
+
+    label_username = tk.Label(root, text="Enter Username:")
+    label_username.grid(row=1, column=0, sticky=tk.E)
+
+    entry_username = tk.Entry(root, width=30)
+    entry_username.grid(row=1, column=1, columnspan=2)
+
+
+    label_password_length = tk.Label(root, text="Enter Password Length:")
+    label_password_length.grid(row=2, column=0, sticky=tk.E)
+
+    entry_password_length = tk.Entry(root, width=10)
+    entry_password_length.grid(row=2, column=1)
+
+
+    label_generated_password = tk.Label(root, text="Generated Password:")
+    label_generated_password.grid(row=3, column=0, sticky=tk.E)
+
+    entry_generated_password = tk.Entry(root, width=30)
+    entry_generated_password.grid(row=3, column=1, columnspan=2)
